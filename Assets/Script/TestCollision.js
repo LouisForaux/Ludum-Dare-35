d525eeed442b8ed8a7c05263b092a9167f22b178
Louis Foraux <louis.foraux@gmail.com>
2016-04-16 16:38:27 +0200

    Upload Dev Branch
    Upload of the Dev Branch to GitHub for a Cloud Build

---
+++ Assets/Script/TestCollision.js
@@ -0,0 +1,8 @@
+﻿var coli:boolean;
+function OnCollisionStay(collision : Collision) {
+    for (var contact : ContactPoint in collision.contacts) {
+        if(contact.otherCollider.name!=""){
+        coli=true;
+    }
+    }
+}
---
+++ Assets/Script/TestModificationObject.js
@@ -0,0 +1,31 @@
+﻿var objsel:GameObject;
+var ligne:Vector3;
+var ray : Ray;
+var hit : RaycastHit;
+var choisi:boolean;
+var materielselectionnee:Material;
+var hehe: GameObject[];
+function Update () {
+    if(Input.GetButtonDown("Fire1")){
+        if(choisi==false){
+            ray = GetComponent.<Camera>().main.ScreenPointToRay (Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
+            if (Physics.Raycast (ray.origin, ray.direction, hit, 100)){
+                objsel=hit.collider.gameObject;
+                objsel.GetComponent.<Renderer>().material = materielselectionnee;
+                var Colision:BoxCollider=objsel.GetComponent("BoxCollider");
+                Colision.enabled= false;
+                for(var fooObj : GameObject in GameObject.FindGameObjectsWithTag("Cube")){
+                fooObj.gameObject.SetActiveRecursively(true);
+        }
+        objsel.transform.Find("TestCollision1.1").gameObject.SetActiveRecursively(false);
+        objsel.transform.Find("TestCollision1.2").gameObject.SetActiveRecursively(false);
+        objsel.transform.Find("TestCollision1.3").gameObject.SetActiveRecursively(false);
+        objsel.transform.Find("TestCollision1.4").gameObject.SetActiveRecursively(false);
+        objsel.transform.Find("TestCollision1.5").gameObject.SetActiveRecursively(false);
+        objsel.transform.Find("TestCollision1.6").gameObject.SetActiveRecursively(false);
+        choisi=true;
+    }
+}
+}
+}
