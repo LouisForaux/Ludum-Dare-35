bab23ddab8a8007e5f2354eb19ac2daff3f4c76d
3t3a <3t3a@outlook.fr>
2016-04-16 17:35:51 +0200

    Prototype change cube 0.2

--- LD35/Assets/Script/TestCollision.js
+++ LD35/Assets/Script/TestCollision.js
@@ -1,8 +1,6 @@
 ﻿var coli:boolean;
 function OnCollisionStay(collision : Collision) {
     for (var contact : ContactPoint in collision.contacts) {
-        if(contact.otherCollider.name!=""){
         coli=true;
-    }
     }
 }
--- LD35/Assets/Script/TestModificationObject.js
+++ LD35/Assets/Script/TestModificationObject.js
@@ -4,29 +4,30 @@ var ray : Ray;
 var hit : RaycastHit;
 var choisi:boolean;
 var materielselectionnee:Material;
-var hehe: GameObject[];
+var Test:Script;
 function Update () {
     if(Input.GetButtonDown("Fire1")){
         if(choisi==false){
             ray = GetComponent.<Camera>().main.ScreenPointToRay (Input.mousePosition);
             Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
             if (Physics.Raycast (ray.origin, ray.direction, hit, 100)){
-                for(var fooObj : GameObject in GameObject.FindGameObjectsWithTag("Cube")){
-                fooObj.gameObject.SetActiveRecursively(true);
-                }
-
             objsel=hit.collider.gameObject;
             objsel.GetComponent.<Renderer>().material = materielselectionnee;
-            var Colision:BoxCollider=objsel.GetComponent("BoxCollider");
-            Colision.enabled= false;
-            objsel.transform.Find("TestCollision1.1").gameObject.SetActiveRecursively(false);
-            objsel.transform.Find("TestCollision1.2").gameObject.SetActiveRecursively(false);
-            objsel.transform.Find("TestCollision1.3").gameObject.SetActiveRecursively(false);
-            objsel.transform.Find("TestCollision1.4").gameObject.SetActiveRecursively(false);
-            objsel.transform.Find("TestCollision1.5").gameObject.SetActiveRecursively(false);
-            objsel.transform.Find("TestCollision1.6").gameObject.SetActiveRecursively(false);
-            choisi=true;
+            for(var fooObj : GameObject in GameObject.FindGameObjectsWithTag("Cube")){
+                fooObj.gameObject.SetActiveRecursively(true);
             }
-        }
+            var C1: GameObject=objsel.transform.Find("TestCollision1.1").gameObject;
+            var C2: GameObject=objsel.transform.Find("TestCollision1.2").gameObject;
+            var C3: GameObject=objsel.transform.Find("TestCollision1.3").gameObject;
+            var C4: GameObject=objsel.transform.Find("TestCollision1.4").gameObject;
+            var C5: GameObject=objsel.transform.Find("TestCollision1.5").gameObject;
+            var C6: GameObject=objsel.transform.Find("TestCollision1.6").gameObject;
+            choisi=true;
+            TestC1.GetComponent("TestCollision");
+            }
     }
+    if(C1.GetComponent(TestCollision).Coli==false){
+        print("Bg de la vie yay :3:3");
+    }
+}
 }
